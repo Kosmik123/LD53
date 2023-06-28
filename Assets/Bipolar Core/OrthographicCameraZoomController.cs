3190160d06b453ac6e0864f39d621c11299e6835
Michal <malpa.w.klapkach@gmail.com>
2023-06-28 22:51:00 +0200

    Update Bipolar Core as submodule

---
+++ Assets/Bipolar Core/OrthographicCameraZoomController.cs
@@ -0,0 +1,50 @@
+using Bipolar.Core.Input;
+using UnityEngine;
+#if NAUGHTY_ATTRIBUTES
+using NaughtyAttributes;
+#endif
+
+public class OrthographicCameraZoomController : MonoBehaviour
+{
+    [SerializeField]
+    private new Camera camera;
+    [SerializeField]
+    private Object axisProvider;
+    public IAxisInputProvider AxisProvider
+    {
+        get => axisProvider as IAxisInputProvider;
+        set
+        {
+            axisProvider = (Object)value;
+        }
+    }
+
+    [SerializeField]
+    private float minSize;
+    [SerializeField]
+    private float maxSize;
+    [SerializeField]
+    private float zoomSpeed;
+
+#if NAUGHTY_ATTRIBUTES
+    [ShowNonSerializedField, ReadOnly]
+#endif
+    private float currentZoom;
+
+    private void Start()
+    {
+        currentZoom = camera.orthographicSize;
+    }
+
+    private void OnEnable()
+    {
+        currentZoom = camera.orthographicSize;
+    }
+
+    private void Update()
+    {
+        currentZoom += zoomSpeed * AxisProvider.GetAxis();
+        currentZoom = Mathf.Clamp(currentZoom, minSize, maxSize);
+        camera.orthographicSize = currentZoom;
+    }
+}
